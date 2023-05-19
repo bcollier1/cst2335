ae1a9e2561dfa810a7a5687c6c23232d80ff02f8
bcollier1 <brady_c_223@hotmail.com>
2023-05-19 10:11:12 -0400

    Implement a new widget that allows to be edit and implemented an onclick button to change the textview based on the newly created widget

--- BradysAndroidLabs/app/src/main/java/algonquin/cst2335/coll0513/MainActivity.java
+++ BradysAndroidLabs/app/src/main/java/algonquin/cst2335/coll0513/MainActivity.java
@@ -3,6 +3,10 @@ package algonquin.cst2335.coll0513;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView mytext = findViewById(R.id.textview);
+        final Button btn = findViewById(R.id.mybutton);
+        EditText myedit = findViewById(R.id.myedittext);
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String editString = myedit.getText().toString();
+                mytext.setText( "Your edit text has: " + editString);
+            }
+        });
     }
 }
