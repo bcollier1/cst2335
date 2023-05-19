c6d086b3b53bca66e9c939ae060663c6f3b9f848
bcollier1 <brady_c_223@hotmail.com>
2023-05-19 10:34:31 -0400

    Implemented lamba method in the OnClicklistener to eliminate redunancy and clean up code. This also allows to remove null exceptions.

--- BradysAndroidLabs/app/src/main/java/algonquin/cst2335/coll0513/MainActivity.java
+++ BradysAndroidLabs/app/src/main/java/algonquin/cst2335/coll0513/MainActivity.java
@@ -7,21 +7,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import algonquin.cst2335.coll0513.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding variableBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        TextView mytext = findViewById(R.id.textview);
-        final Button btn = findViewById(R.id.mybutton);
-        EditText myedit = findViewById(R.id.myedittext);
-        btn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String editString = myedit.getText().toString();
-                mytext.setText( "Your edit text has: " + editString);
-            }
-        });
+
+        variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(variableBinding.getRoot());
+
+        TextView mytext =   variableBinding.textview;
+        final Button btn =   variableBinding.mybutton;
+        EditText myedit =   variableBinding.myedittext;
+        String editString = myedit.getText().toString();
+        btn.setOnClickListener(
+                vw ->   mytext.setText(String.format("Your edit text has: %s", editString)) );
     }
 }
