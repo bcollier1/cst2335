1eafbd57bd8047491f7f1da186cd70203aa7ff66
bcollier1 <brady_c_223@hotmail.com>
2023-05-20 18:11:53 -0400

    Implemented compound buttons with the feature to turn on/off widget based on the status of the other widgets

--- BradysAndroidLabs/app/src/main/java/algonquin/cst2335/coll0513/ui/MainActivity.java
+++ BradysAndroidLabs/app/src/main/java/algonquin/cst2335/coll0513/ui/MainActivity.java
@@ -1,11 +1,14 @@
 package algonquin.cst2335.coll0513.ui;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
+
 import algonquin.cst2335.coll0513.data.MainViewModel;
 import algonquin.cst2335.coll0513.databinding.ActivityMainBinding;
 
@@ -25,10 +28,39 @@ public class MainActivity extends AppCompatActivity {
 
         TextView textview = variableBinding.textview;
         variableBinding.mybutton.setOnClickListener(vw -> {
-            model.editString.postValue(variableBinding.myedittext.getText().toString());
-            model.editString.observe(this, s ->{
+            model.editString.observe(this, s -> {
                 variableBinding.textview.setText(String.format("Your edit text has: %s", s));
             });
+            model.editString.postValue(variableBinding.myedittext.getText().toString());
+        });
+
+        model.isSelected.observe(this, selected -> {
+            Context context = getApplicationContext();
+            int duration = Toast.LENGTH_SHORT;
+            CharSequence text = ("The value is now: " + selected);
+            variableBinding.checkbox.setChecked(selected);
+            variableBinding.radio.setChecked(selected);
+            variableBinding.switchbutton.setChecked(selected);
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        });
+
+        variableBinding.checkbox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            model.isSelected.postValue(isChecked);
+            variableBinding.radio.setChecked(isChecked);
+            variableBinding.switchbutton.setChecked(isChecked);
+        });
+
+        variableBinding.switchbutton.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            model.isSelected.postValue(isChecked);
+            variableBinding.checkbox.setChecked(isChecked);
+            variableBinding.radio.setChecked(isChecked);
+        });
+
+        variableBinding.radio.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            model.isSelected.postValue(isChecked);
+            variableBinding.checkbox.setChecked(isChecked);
+            variableBinding.switchbutton.setChecked(isChecked);
         });
     }
 }
